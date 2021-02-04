174aecbfb42b6b2a26633656ed82798b0ff6769d
Danny Guerrero <dannyguerrero2@gmail.com>
2021-02-03 22:53:10 -0600

    Fix bug collapsible clickable when toggler not present.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-const collapsibles = document.querySelectorAll('.nav__toggler');
+document.querySelector('.nav__toggler').addEventListener('click', function () {
+  this.parentElement.classList.toggle('collapsible--expanded');
+});
+
+const collapsibles = document.querySelectorAll('.collapsible__header');
 collapsibles.forEach((item) =>
   item.addEventListener('click', function () {
-    console.log(this.parentElement);
+    console.log('hi');
     this.parentElement.classList.toggle('collapsible--expanded');
   })
 );
